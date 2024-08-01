6f1ae8dd470cf9d2c3b1be5eff08190a8c8d7898
Gabriel Biankati <gabriel.2023014161@alunocesul.com>
2024-08-01 20:19:03 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GerenciamentoVeiculos
+{
+    public class Veiculo
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+    
+
+        public Veiculo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
+
+        public virtual void ExibirDetalhes()
+        {
+            Console.WriteLine($"Marca: {Marca}, Modelo {Modelo}");
+        }
+    }
+
+    public class Carro : Veiculo
+    {
+        public int NumeroDePortas { get; set; }
+
+        public Carro(string marca, string modelo, int numeroDePortas) : base(marca, modelo)
+        {
+            this.NumeroDePortas = numeroDePortas;
+        }
+
+        public override void ExibirDetalhes()
+        {
+            base.ExibirDetalhes();
+            Console.WriteLine($"Numero de portas é {NumeroDePortas}.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Veiculo meuCarro = new Carro("Ford", "Fiesta", 4);
+            meuCarro.ExibirDetalhes();
+        }
+    }
+}
